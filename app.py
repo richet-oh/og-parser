50c7fef13fbabf3a27cb67957b68763db74a094f
richet-oh <richet@bucketplace.net>
2025-01-08 14:47:46 +0900

    parser

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ USER_AGENTS = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59'
 ]
 
-def parse_og_metadata(url, retry_count=3):
+def parse_og_metadata(url, retry_count=10):
     for attempt in range(retry_count):
         try:
             headers = {
