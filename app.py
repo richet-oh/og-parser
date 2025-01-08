06f705a143015c097b71660aabc0b4d163917223
richet-oh <richet@bucketplace.net>
2025-01-08 14:46:26 +0900

    parser

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import random
 import urllib3
-import time
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # List of common user agents for rotation
